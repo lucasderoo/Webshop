6643515308cc54d4df952504802e54c260747d0f
chevery <cheobia@icloud.com>
2018-10-11 11:37:10 +0200

    Product page

--- resources/views/shop/show.blade.php
+++ resources/views/shop/show.blade.php
@@ -17,7 +17,7 @@ height: 25px;
 	margin-bottom: 15px;
 }
 .main-image-buy-btn{
-	width: 74%; 
+	width: 74%;
 	height: 500px;
 	float: left;
 }
@@ -26,12 +26,14 @@ height: 25px;
 	margin: auto;
 }
 .buy-buttons{
-	margin-top: 80px;
+	margin-top: 40px;
+	
 }
 .buy-buttons button{
 	color: white;
 	width: 40%;
 	height: 50px;
+
 }
 .add-to-cart{
 	float: left;
@@ -42,12 +44,34 @@ height: 25px;
 	background-color: #FF9900;
 }
 .main-image-buy-btn-wrapper{
-	width: 85%;
+	width: 100%;
     margin: auto;
 }
 .top-row{
 	margin-top: 10px;
 	height: 50px;
+}
+
+.Big_image{
+max-width: 600px;
+max-height: 700px;
+
+}
+.normal_sized_p{
+font-size: 18px
+
+}
+.x100_pic{
+	max-width: 100px;
+	max-height: 100px;
+
+}
+
+b{
+font-size: 20px;
+}
+hr{
+ border-color:black;
 }
 h5{
 	color: #00ACED;
@@ -55,28 +79,32 @@ h5{
 .bottom-p{
 	width: 100% !important;
 }
+
+
 </style>
-<div class="container-fluid main-container" style="max-width: 1000px; margin: auto; margin-top: 100px;">
+<div class="container-fluid main-container" style="max-width: 1300px; margin: auto; margin-top: 100px;">
     <div class="row top-row">
         <div class="col-md-8">
-        	<p>Home > {{$product->productable->carrier->name}} > {{$product->productable->artist->name}} - {{$product->title}}</p>
+        	<p>Home > {{$product->productable->genre->name}} > {{$product->productable->artist->name}} - {{$product->title}}</p>
         </div>
         <div class="col-md-4">
         	<h5>{{$product->productable->artist->name}} - {{$product->title}}</h5>
         </div>
     </div>
-    <div class="row">
+    <div class="row justify-content-between">
         <div class="col-md-8">
         	<div class="image-select">
-        		<img src="https://via.placeholder.com/80x60">
-        		<img src="https://via.placeholder.com/80x60">
-        		<img src="https://via.placeholder.com/80x60">
-        		<img src="https://via.placeholder.com/80x60">
+
+        		<img class="x100_pic" src="{{$product->main_image_url}}">
+        		<img class="x100_pic"  src="https://img.cdandlp.com/2016/06/imgL/118209175-2.jpg">
+        		<img class="x100_pic"  src="https://via.placeholder.com/100x100">
+        		<img class="x100_pic"  src="https://via.placeholder.com/100x100">
         	</div>
         	<div class="main-image-buy-btn">
         		<div class="main-image-buy-btn-wrapper">
-        			<img src="https://via.placeholder.com/400x300">
-
+        		<!--	<img src="https://via.placeholder.com/600x700">
+-->
+							<img class="Big_image" src="{{$product->main_image_url}}">
         			<div class="buy-buttons">
         				<button class="add-to-cart btn">Add to Cart</button>
         				<button class="buy-now btn">Buy Now</button>
@@ -85,19 +113,27 @@ h5{
         	</div>
         </div>
         <div class="col-md-4">
+					<b> €{{$product->price}}</b>
+					<hr>
+
+<div class="product-info">
+					<h5>Seller </h5><p >{{$product->productable->carrier->name}}</p>
+</div>
+					<hr>
         	<h5>Product description</h5>
         	<div class="product-info">
-        	<p>Artist: {{$product->productable->artist->name}}</p>
-        	<p>Title: {{$product->title}}</p>
-        	<p>Format: {{$product->productable->carrier->name}}</p>
-        	<p>Genre: {{$product->productable->genre->name}}</p>
-        	<p class="bottom-p">Released: {{$product->productable->release_date}}</p>
+        	<p> <b>Artist:</b> {{$product->productable->artist->name}}</p>
+        	<p><b>Title:</b> {{$product->title}}</p>
+        	<p><b>Format:</b> {{$product->productable->carrier->name}}</p>
+        	<p><b>Genre:</b> {{$product->productable->genre->name}}</p>
+        	<p class="bottom-p"><b>Released:</b> {{$product->productable->release_date}}</p>
         	</div>
 
         	<div class="product-desc">
-        	<br>
+
         		<p>{{$product->productable->description}}<p>
         	</div>
+					<hr>
         </div>
     </div>
-</div> 
+</div>
