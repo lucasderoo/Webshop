ca06982bc61cf34d493dcee9cddb71689544b84c
Lou-Anne <0950196@hr.nl>
2018-10-11 11:44:04 +0200

    Homepage begin

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.header')
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+
+<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+
+<link rel="stylesheet" href="css/main.css">
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-6 offset-md-4">
+      <div id="carouselHomePage1" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-10" src="https://via.placeholder.com/400x300" alt="First slide">
+            <div class="carousel-caption">
+              <<h1>AC/DC Sale</h1>
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-10" src="http://www.tutoriels-animes.com/photos/redimensionnements/400X300.jpg" alt="Second slide">
+            <div class="carousel-caption">
+              <<h1>Metallica Sale</h1>
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-10" src="https://via.placeholder.com/400x300" alt="Third slide">
+            <div class="carousel-caption">
+              <<h1>Sale</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
