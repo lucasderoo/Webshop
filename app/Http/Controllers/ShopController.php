8569fea0a01099fb3e84dc46fcefcad33d0300cd
Lucas <99028091@mydavinci.nl>
2018-10-15 09:51:35 +0200

    Added default value for minprice and maxprice
    - Added default value minprice and maxprice
    - Changed nullable to main_image_url

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -60,8 +60,11 @@ class ShopController extends Controller
         $musicProducts = $musicQuery->pluck('id');
 
         // productQuery filters here
-        $productQuery = $productQuery->where('price', '>=', (int)$request->get('min-price'))
-                                     ->where('price', '<=', (int)$request->get('max-price'));
+        $minPrice = $request->has('min-price') ? $request->get('min-price') : 0;
+        $maxPrice = $request->has('max-price') ? $request->get('max-price') : (int)Product::max('price');
+
+        $productQuery = $productQuery->where('price', '>=', $minPrice)
+                                     ->where('price', '<=', $maxPrice);
 
 
 
