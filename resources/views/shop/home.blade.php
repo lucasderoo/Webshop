4d99073cd68ac3c6f5a5bfc053a90fe357b42ddc
Lou-Anne <0950196@hr.nl>
2018-11-12 11:47:11 +0100

    Homepage with carousel
    as the title says

--- resources/views/shop/home.blade.php
+++ resources/views/shop/home.blade.php
@@ -35,8 +35,32 @@ body{
   <div class="row">
     <div class="col-md-8 offset-md-2">
       <div class="row">
-        <div class ="main-story col-12">
-        </div><!--main-story-->
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100" src="images/bg-banner-metallica.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="images/bg-banner-metallica.jpg" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="images/bg-banner-metallica.jpg" alt="Third slide">
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
       </div>
       <div class="row">
         <div class ="two-small-banners col-12 offset-md-2">
