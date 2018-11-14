b40baf63f700bfa25942b1fa04985844ce543cf6
Jered <jeredsloof@hotmail.com>
2018-11-14 10:22:21 +0100

    404 Page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,16 @@
 @include('layouts.header')
 
-<h1>404 Page</h1>
+<div class="page-wrap d-flex flex-row align-items-center">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 text-center">
+			<br><br><br>
+                <span class="display-1 d-block">404</span>
+                <div class="mb-4 lead">The page you are looking for was not found.</div>
+				
+            </div>
+        </div>
+    </div>
+</div>
 
 @include('layouts.footer')
