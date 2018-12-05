280e9ce9731c97464badbe79426a68e9a084cce7
Lou-Anne <0950196@hr.nl>
2018-12-05 11:11:22 +0100

    Generic template inserted for Customer service

--- resources/views/shop/customerservice.blade.php
+++ resources/views/shop/customerservice.blade.php
@@ -9,8 +9,8 @@
         <div class="col-sm-12">
           <div class="jumbotron jumbotron-fluid">
             <div class="container">
-              <h1 class="display-4">Latest News</h1>
-              <p class="lead">Check here to see the latest adaptations, additions and changes to the webshop</p>
+              <h1 class="display-4">Customer Service</h1>
+              <p class="lead">If you have a question for our customer service, feel free to contact us!</p>
             </div>
           </div>
         </div>
@@ -20,14 +20,61 @@
     </div>
   </div>
 </div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            <div class="well well-sm">
+                <form>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="name">
+                                Name</label>
+                            <input type="text" class="form-control" id="name" placeholder="Enter name" required="required" />
+                        </div>
+                        <div class="form-group">
+                            <label for="email">
+                                Email Address</label>
+                            <div class="input-group">
+                                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
+                                </span>
+                                <input type="email" class="form-control" id="email" placeholder="Enter email" required="required" /></div>
+                        </div>
+</div>
 
-<h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc mattis enim ut tellus elementum. Urna porttitor rhoncus dolor purus non enim praesent. Vitae nunc sed velit dignissim sodales ut eu sem. Venenatis lectus magna fringilla urna porttitor. Ullamcorper morbi tincidunt ornare massa eget egestas purus. Dui vivamus arcu felis bibendum. Cum sociis natoque penatibus et magnis dis. Leo vel orci porta non pulvinar neque laoreet suspendisse interdum. Aliquet eget sit amet tellus cras adipiscing. Lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi. Quis ipsum suspendisse ultrices gravida dictum. Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique.
-
-Aliquet eget sit amet tellus cras adipiscing enim. Scelerisque varius morbi enim nunc faucibus a. Est velit egestas dui id ornare arcu. Et molestie ac feugiat sed lectus vestibulum mattis. Amet justo donec enim diam vulputate ut. Ultricies integer quis auctor elit sed vulputate. Feugiat scelerisque varius morbi enim. Malesuada fames ac turpis egestas integer eget aliquet nibh praesent. Urna duis convallis convallis tellus id. Sit amet purus gravida quis blandit turpis. Tellus in metus vulputate eu scelerisque felis imperdiet. Enim facilisis gravida neque convallis a cras semper auctor neque. Metus dictum at tempor commodo ullamcorper. Enim ut tellus elementum sagittis vitae. Cursus sit amet dictum sit amet justo donec enim diam. Neque sodales ut etiam sit amet nisl purus in mollis. Nunc mattis enim ut tellus elementum sagittis vitae et. Venenatis a condimentum vitae sapien pellentesque habitant morbi tristique.
-
-Neque vitae tempus quam pellentesque nec. Nec ultrices dui sapien eget. Ac ut consequat semper viverra. Vel orci porta non pulvinar neque laoreet suspendisse. Odio ut enim blandit volutpat. In nibh mauris cursus mattis molestie a. Sit amet purus gravida quis blandit turpis. Semper feugiat nibh sed pulvinar proin gravida. Porttitor massa id neque aliquam vestibulum morbi blandit cursus risus. Suspendisse sed nisi lacus sed viverra tellus. Velit aliquet sagittis id consectetur purus ut faucibus pulvinar. Varius quam quisque id diam vel quam elementum pulvinar. Non diam phasellus vestibulum lorem sed risus ultricies tristique. Arcu non odio euismod lacinia at. Aliquet nec ullamcorper sit amet risus nullam eget. Quam adipiscing vitae proin sagittis. Nullam vehicula ipsum a arcu cursus vitae congue mauris rhoncus. Eget mi proin sed libero enim sed faucibus turpis.
-
-Suspendisse ultrices gravida dictum fusce ut placerat orci. Laoreet sit amet cursus sit amet dictum sit amet justo. Ornare quam viverra orci sagittis eu volutpat. Felis eget velit aliquet sagittis id consectetur purus. Tempus iaculis urna id volutpat lacus laoreet non curabitur. At erat pellentesque adipiscing commodo elit at. Mus mauris vitae ultricies leo integer malesuada nunc. Convallis a cras semper auctor neque vitae tempus quam. Aliquet enim tortor at auctor. Bibendum neque egestas congue quisque egestas diam in arcu cursus.
-
-Nisl rhoncus mattis rhoncus urna neque viverra justo nec ultrices. Diam sollicitudin tempor id eu nisl nunc. Est ultricies integer quis auctor elit sed vulputate. Interdum consectetur libero id faucibus. Tristique senectus et netus et malesuada fames ac turpis. Et netus et malesuada fames ac turpis egestas sed tempus. Sollicitudin tempor id eu nisl nunc mi ipsum. Morbi tempus iaculis urna id volutpat lacus laoreet. Dignissim suspendisse in est ante in nibh. Pellentesque habitant morbi tristique senectus et.</h4>
+<div class="col-md-6">
+    <div class="form-group">
+        <label for="name">
+            Message</label>
+        <textarea name="message" id="message" class="form-control" rows="9" cols="25" required="required"
+            placeholder="Message"></textarea>
+    </div>
+</div>
+<div class="col-md-12">
+    <button type="submit" class="btn btn-primary pull-right" id="btnContactUs">
+        Send Message</button>
+</div>
+</div>
+</form>
+</div>
+</div>
+<div class="col-md-4">
+<form>
+<legend><span class="glyphicon glyphicon-globe"></span> Our office</legend>
+<address>
+<strong>MUZEN Inc.</strong><br>
+795 Folsom Ave, Suite 600<br>
+San Francisco, CA 94107<br>
+<abbr title="Phone">
+P:</abbr>
+(123) 456-7890
+</address>
+<address>
+<strong>Full Name</strong><br>
+<a href="mailto:#">first.last@example.com</a>
+</address>
+</form>
+</div>
+</div>
+</div>
 @include('layouts.footer')
