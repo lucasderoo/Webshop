d2b09e5c58c2155152bd01b0cd928b5d8105c076
Jered <jeredsloof@hotmail.com>
2019-01-11 12:26:13 +0100

    Default image on confirm page

--- resources/views/shop/checkout/confirm.blade.php
+++ resources/views/shop/checkout/confirm.blade.php
@@ -119,9 +119,9 @@
                     <div class="col-md-12 cart-product">
                         <div class="cart-product-image">
                             @if(Auth::guest())
-                            <img src="{{ asset('images/uploads/products/product_').$key.'/img_'.$product['main_image_url'].'.png' }}" id="afbeelding">
+                            <img src="{{ asset('images/uploads/products/product_').$key.'/img_'.$product['main_image_url'].'.png' }}" class="afbeelding" id="afbeelding">
                             @else
-                            <img src="{{ asset('images/uploads/products/product_').$product->product->id.'/img_'.$product->product->main_image_url.'.png'}}" id="afbeelding">
+                            <img src="{{ asset('images/uploads/products/product_').$product->product->id.'/img_'.$product->product->main_image_url.'.png'}}" class="afbeelding" id="afbeelding">
                             @endif
                         </div>
                         <div class="cart-product-info">
@@ -145,5 +145,13 @@
         </div>
     </div>
 </div>
+
+<script>
+$('.afbeelding').each(function(){
+    jQuery(this)[0].onerror = function() {
+        jQuery(this)[0].src = "{{ asset('images/stock_image.png') }}";
+    }
+});
+</script>
 
 @include('layouts.footer')
